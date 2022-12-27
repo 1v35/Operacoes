a971fea4b84c024fb244cf8213aca68fed097e24
1v35 <ivaldo.raul@hotmail.com>
2022-12-26 21:27:03 -0300

    lib operacoes matematicas

---
+++ src/main/java/Operacoes.java
@@ -0,0 +1,19 @@
+public class Operacoes {
+
+    public int soma (int x, int y){
+        return x + y;
+    }
+
+    public int sub (int x, int y){
+        return x - y;
+    }
+
+    public int multi (int x, int y){
+        return x * y;
+    }
+
+    public int div (int x, int y){
+        return x / y;
+    }
+    
+}
